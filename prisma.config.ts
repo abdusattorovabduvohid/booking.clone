22f254987b1e55f71975b6afb49f79648b37eec0
Ibso77 <ibrohimjonsobirov68@gmail.com>
2026-05-19 18:43:28 +0500

    eror fixing

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,9 +4,8 @@ import { defineConfig, env } from "prisma/config";
 export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
-    url: env(
-      "DATABASE_URL",
-      "postgresql://localhost:5432/booking_clone?schema=public"
-    ),
+    url: process.env.DATABASE_URL
+      ? env("DATABASE_URL")
+      : "postgresql://localhost:5432/booking_clone?schema=public",
   },
 });
